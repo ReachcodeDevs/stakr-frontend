d2281e6673e7b349c265fcfb701523db0ef38b7a
Luqmaan.A <sri.luqmaanahmed@gmail.com>
2022-03-30 03:14:17 +0100

    Added (working) Sign In page and validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'theme_card.dart';
@@ -11,11 +13,36 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  runApp(const MyApp());
+  runApp(const AuthGate());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return MaterialApp(title: "Stakr", theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Montserrat'), home:const SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration(),
+              GoogleProviderConfiguration(
+                  clientId: "1:43501228528:web:0d1651f94bb2bc8ab8cd35")
+            ],
+          ));
+        }
+
+        // Render your application if authenticated
+        return const TheApp();
+      },
+    );
+  }
+}
+
+class TheApp extends StatelessWidget {
+  const TheApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -25,11 +52,39 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Montserrat'),
       home: const Page(title: "Home", child: HomePage()),
       routes: <String, WidgetBuilder>{
-        '/portfolio': (BuildContext context) => const Page(child: PortfolioPage(), title: "Stake Breakdown"),
-        '/theme': (BuildContext context) => const Page(child: ThemePage(), title: ""),
-        '/stake':(BuildContext context) => const Page(child: StakePage(), title: "",),
-        '/donations': (BuildContext context) => const Page(child: DonationsPage(), title: "Donation History",)
+        '/portfolio': (BuildContext context) =>
+            const Page(child: PortfolioPage(), title: "Stake Breakdown"),
+        '/theme': (BuildContext context) =>
+            const Page(child: ThemePage(), title: ""),
+        '/stake': (BuildContext context) => const Page(
+              child: StakePage(),
+              title: "",
+            ),
+        '/donations': (BuildContext context) => const Page(
+              child: DonationsPage(),
+              title: "Donation History",
+            )
       },
+    );
+  }
+}
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return SignInScreen(
+      providerConfigs: [
+        EmailProviderConfiguration(),
+        GoogleProviderConfiguration(
+            clientId: "1:43501228528:web:0d1651f94bb2bc8ab8cd35"),
+      ],
     );
   }
 }
@@ -64,7 +119,7 @@ class Page extends StatelessWidget {
             backgroundColor: Colors.grey,
           ),
         ],
-        ),
+      ),
     );
   }
 }
@@ -107,30 +162,52 @@ class HomePage extends StatelessWidget {
                 image: AssetImage("images/Rectangle5.png"),
                 fit: BoxFit.contain,
               ),
-              SizedBox(child: Row(
-                children: [
-                  Column(children: [
-                    const Text("Total Donated "),
-                    Container(child: RichText(text: 
-                    const TextSpan(children: 
-                    [
-                      WidgetSpan(child: Icon(Icons.currency_pound_rounded, size: 20,)),
-                      TextSpan(text: "365", style: TextStyle(color: Colors.white)),
-                    ])), margin: const EdgeInsets.all(10),) 
-                  ],),
-                   Column(children: [
-                    const Text("Tokens Staked"),
-                    Container(child: RichText(text: 
-                    const TextSpan(children: 
-                    [
-                      WidgetSpan(child: Icon(Icons.token, size: 20,)),
-                      TextSpan(text: "100", style: TextStyle(color: Colors.white)),
-                    ])), margin: const EdgeInsets.all(10),) 
-                  ],),
-                ],
-              ),
-              width: 210,
-              height: 100,)
+              SizedBox(
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        const Text("Total Donated "),
+                        Container(
+                          child: RichText(
+                              text: const TextSpan(children: [
+                            WidgetSpan(
+                                child: Icon(
+                              Icons.currency_pound_rounded,
+                              size: 20,
+                            )),
+                            TextSpan(
+                                text: "365",
+                                style: TextStyle(color: Colors.white)),
+                          ])),
+                          margin: const EdgeInsets.all(10),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("Tokens Staked"),
+                        Container(
+                          child: RichText(
+                              text: const TextSpan(children: [
+                            WidgetSpan(
+                                child: Icon(
+                              Icons.token,
+                              size: 20,
+                            )),
+                            TextSpan(
+                                text: "100",
+                                style: TextStyle(color: Colors.white)),
+                          ])),
+                          margin: const EdgeInsets.all(10),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                width: 210,
+                height: 100,
+              )
             ],
           ),
         ),
@@ -202,46 +279,38 @@ class _ThemeCategoryState extends State<ThemeCategory> {
   }
 }
 
-
 class PortfolioPage extends StatelessWidget {
-  const PortfolioPage({ Key? key }) : super(key: key);
+  const PortfolioPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
 
 class ThemePage extends StatelessWidget {
-  const ThemePage({ Key? key }) : super(key: key);
+  const ThemePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
 
 class StakePage extends StatelessWidget {
-  const StakePage({ Key? key }) : super(key: key);
+  const StakePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
 
 class DonationsPage extends StatelessWidget {
-  const DonationsPage({ Key? key }) : super(key: key);
+  const DonationsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    );
+    return Container();
   }
 }
